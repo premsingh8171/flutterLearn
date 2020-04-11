51bd1b27470248adbefba9634dafabbf10cfe44b
prem <premsingh8171@gmail.com>
2020-04-11 19:53:00 +0530

    learn container

--- lib/main.dart
+++ lib/main.dart
@@ -1,94 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Flutter first app"),
+  home: Home(),
+));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text('my first app'),
           centerTitle: true,
-          backgroundColor: Colors.red[600],
-        ),
-        body: Center(
-          child: FlatButton(
-              onPressed: () {},
-              child: Text('click me again'),
-              color: Colors.amber),
-
-//            FlatButton(
-//              onPressed: (){
-//                print('you click me');
-//              },
-//              child: Text("click me"),
-//              color: Colors.blue,
-//            ),
-
-//            Icon(
-//                  Icons.airport_shuttle,
-//              color: Colors.blue,
-//              size: 50.0,
-//            ),
-
-// ********************* how to show custom image assets folder ********************
-          //       Image.asset('assets/ic_launcher.png'),
-//            Image(
-//          image: AssetImage('assets/pic1.jpg'),
-
-// ********************** here we learn how to show network image ***************************
-//            image: NetworkImage('https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg'),
-
-// ****************************** here we learn Text ***************************
-//          Text(
-//            "Hello developer!",
-//            style: TextStyle(
-//                fontSize: 20.0,
-//                fontWeight: FontWeight.bold,
-//                letterSpacing: 2.0,
-//                color: Colors.grey[600],
-//                fontFamily:'Nutino',
-//
-//            ),
-//          ),
-        ),
-        backgroundColor: Colors.white,
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          backgroundColor: Colors.red[600],
-        ),
+          backgroundColor: Colors.red[600]
       ),
-    ));
+      body: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Text('hello, again')
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red[600],
+        child: Text('click'),
+      ),
+    );
+  }
+}
 
-// snippets for icons and buttons
+// snippets for padding & margin
 
-//  Icon(
-//    Icons.airport_shuttle,
-//    color: Colors.lightBlue,
-//    size: 50.0
+//  Container(
+//    margin: EdgeInsets.all(40.0),
+//    padding: EdgeInsets.all(30.0),
+//    color: Colors.grey[400],
+//    child: Text('hey, ninjas!'),
 //  ),
-
-//  RaisedButton(
-//    onPressed: () {
-//      print('you clicked me');
-//    },
-//    child: Text('click me'),
-//    color: Colors.lightBlue,
-//  ),
-
-//  FlatButton(
-//    onPressed: () {},
-//    child: Text('click me again'),
-//    color: Colors.amber
-//  ),
-
-//  RaisedButton.icon(
-//    onPressed: () {},
-//    icon: Icon(Icons.mail),
-//    label: Text('mail me'),
-//    color: Colors.amber,
-//  ),
-
-//IconButton(
-//onPressed: () {
-//print('you clicked me');
-//},
-//icon: Icon(Icons.alternate_email),
-//color: Colors.amber,
-//),
